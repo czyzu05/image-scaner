d7ddd9095fd96885bec05c881819622da2411890
czyzu05 <damian.czyz1998@gmail.com>
2020-08-26 17:18:41 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './App.css'
+
+
+class App extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div className="App">
+        App
+      </div>
+    )
+  }
+}
+
+export default App;
+
