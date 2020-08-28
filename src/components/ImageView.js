df4ad4186fa7c0d85925831344cab6859ca98c20
czyzu05 <damian.czyz1998@gmail.com>
2020-08-28 18:41:16 +0200

    style update

--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -5,12 +5,12 @@ import '../styles/ImageView.css'
 const ImageView = (props) => {
     const { largeImageURL: image, tags, user, pageURL } = props.location.state.image
     return (
-        <div>
-            <img src={image} alt={tags} />
-            <p>&copy; pixabay</p>
-            <h4>Author: <span>{user}</span></h4>
-            <h4>Download: <span><a target="_blank" href={pageURL}>GO TO DOWNLOAD</a></span></h4>
-            <button><Link to="/">GO TO HOME</Link></button>
+        <div className="viewImageWrapper">
+            <img src={image} alt={tags} className="img" />
+            <p className="info">&copy; pixabay</p>
+            <h4 className="author">Author: <span className="authorName">{user}</span></h4>
+            <h4 className="download">Download: <span><a target="_blank" className="downloadLink" href={pageURL}>GO TO DOWNLOAD</a></span></h4>
+            <button className="homeButton"><Link to="/" className="homeLink">GO TO HOME</Link></button>
         </div>
     )
 }
